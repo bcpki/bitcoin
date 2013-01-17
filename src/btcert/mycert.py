47d02f36fa5911b13ae0508f68261dc9d6c45605
Timo Hanke <timo.hanke@web.de>
2013-01-17 15:58:22 +0100

    btcert.proto protobuf definition and example

---
+++ src/btcert/mycert.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+from btcert_pb2 import * 
+import binascii
+
+# fill out a minimal bitcoin cert
+cert = BitcoinCert()
+cert.version = "0.2"
+
+# first the data part (the part is later signed by the "higher level cert")
+cert.data.version = "0.2"
+cert.data.subjectname = 'Foo Inc.'
+email = cert.data.contacts.add()
+email.type = email.EMAIL
+email.value = "foo@fooinc.com"
+paykey = cert.data.paymentkeys.add()
+paykey.usage = paykey.PAYMENT
+paykey.algorithm.type = paykey.algorithm.BTCPKISINGLE # is default anyway
+paykey.algorithm.version = "0.2"
+paykey.value = "0211b60f23135a806aff2c8f0fbbe620c16ba05a9ca4772735c08a16407f185b34".decode('hex')
+
+# see how the cert.data looks
+print "data part of cert"
+print cert.data
+
+# compute hash of data (the signature signs this hash)
+import hashlib 
+def DataToHash(data):
+  return hashlib.sha256(data.SerializeToString()).digest()
+
+# see the hash
+print "hash of data part of cert"
+print DataToHash(cert.data).encode('hex')
+
+# add signature to cert
+sig = cert.signatures.add()
+sig.algorithm.type = sig.algorithm.BTCPKI
+sig.algorithm.version = "0.2"
+sig.value = "foo"  # for signatures of type BTCPKI the alias IS the value, 
+                   # other types place the signature of BitcoinCertDataToHash(certData) here, 
+                   # for BTCPKI this hash appears in the blockchain instead
+
+# see how the cert looks
+print "whole cert"
+print cert
+
+# serialize it
+def CertToAscii(cert):
+  ser = cert.SerializeToString()
+  ver = cert.version
+  crc = binascii.crc32(ser) & 0xffffff # keep only last 24 bit (should use CRC-24 like OpenPGP)
+  # OpenPGP uses initializations for its crc-24, see http://tools.ietf.org/html/rfc2440
+  asc = binascii.b2a_base64(cert.SerializeToString())[:-1]  # without trailing newline
+  asc += '=' # checksum is seperated by =
+  asc += binascii.b2a_base64(('%x'%crc).decode('hex'))
+ 
+  res = '-----BEGIN BTCPKI CERTIFICATE-----\n'
+  res += 'Version: '+cert.version+'\n\n'
+  res += '\n'.join(asc[i:i+72] for i in xrange(0, len(asc), 72))  
+  res += '-----END BTCPKI CERTIFICATE-----\n'
+  return res
+
+print CertToAscii(cert)
+
+# TODO: AsciiToCert
+
+# OLD
+#from subprocess import Popen,PIPE,check_call,call
+#p = Popen(['./bitcoind','-testnet','registeralias','foo3','0.5',hash],stdout=PIPE)
+#result = p.stdout.read()
+#print result
+
+#f=open('my.cert','wb')
+#f.write(Cert.SerializeToString())
+#f.close()
