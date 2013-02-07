17759e13629222797d78c4a61057ee535135d432
Timo Hanke <timo.hanke@web.de>
2013-02-07 11:01:19 +0100

    e.py

--- src/bcert/e.py
+++ src/bcert/e.py
@@ -21,14 +21,26 @@ from ecdsa import numbertheory
 from ecdsa import ecdsa
 from ecdsa import util
 import bitcoin # from electrum code, contains base58 encode and others
-from bitcoin import hash_160 # from electrum code, contains base58 encode and others
-import string
 from hashlib import sha256 # will probably need this too
+from bitcoin import hash_160
+from mycert2 import *
+import binascii
 
-#bitcoin.addrtype = 0x00 # creates mainnet addresses
-bitcoin.addrtype = 0x6f # creates testnet addresses
+versno="0.4"
+basedir="/web/btcrypt.org/public_html/certs/"
 
-# secp256k1, http://www.oid-info.com/get/1.3.132.0.10
+# ----- testnet / mainnet -----
+
+def testnet():
+  bitcoin.addrtype = 0x6f 
+  
+def mainnet():
+  bitcoin.addrtype = 0x00 
+
+testnet() # default
+
+# ----- curve definition secp256k1 -----
+
 _p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2FL
 _r = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141L
 _b = 0x0000000000000000000000000000000000000000000000000000000000000007L
@@ -37,7 +49,9 @@ _Gx = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798L
 _Gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8L
 C = ellipticcurve.CurveFp( _p, _a, _b ) 
 G = ellipticcurve.Point( C, _Gx, _Gy, _r )
-zero = 0*G;
+Z = 0*G
+
+# ----- conversion functions -----
 
 # notion
 ## point = EC point object
@@ -63,16 +77,6 @@ def toraw(s):
   else:
     return s
   
-# convert compressed pubkey (32+1 bytes) to point
-def pubkey2point(pubkey):
-  x = int(tohex(pubkey)[2:],16)
-  fx = (x * x * x + C.a() * x + C.b()) % C.p()
-  y = numbertheory.square_root_mod_prime(fx,C.p())
-  P = ellipticcurve.Point(C,x,y)
-  if point2pubkey(P) != toraw(pubkey):
-      P = ellipticcurve.Point(C,x,C.p()-y)
-  return P
-
 #ECP = ecdsa.Public_key(G,P)
 #ECQ = ecdsa.Public_key(G,Q)
 
@@ -91,6 +95,14 @@ def point2addr(p):
   return bitcoin.public_key_to_bc_address(point2pubkey(p))
 
 # convert pubkey
+def pubkey2point(pubkey):
+  x = int(tohex(pubkey)[2:],16)
+  fx = (x * x * x + C.a() * x + C.b()) % C.p()
+  y = numbertheory.square_root_mod_prime(fx,C.p())
+  P = ellipticcurve.Point(C,x,y)
+  if point2pubkey(P) != toraw(pubkey):
+      P = ellipticcurve.Point(C,x,C.p()-y)
+  return P
 def pubkey2addr(pubkey): 
   return bitcoin.public_key_to_bc_address(toraw(pubkey))
 def pubkey2id(pubkey): 
@@ -133,3 +145,153 @@ def sec2idx(sec):
   return point2idx(sec2point(sec));
 def sec2addr(sec):
   return point2addr(sec2point(sec));
+
+# hash
+def hash2pubkey(hexstr):
+  return point2pubkey(derivepoint(Z,hexstr))
+
+def hash2pubkeyx(hexstr):
+  return point2pubkeyx(derivepoint(Z,hexstr))
+
+def hash2id(hexstr):
+  return point2id(derivepoint(Z,hexstr))
+
+def hash2idx(hexstr):
+  return point2idx(derivepoint(Z,hexstr))
+
+def hash2addr(hexstr):
+  return point2addr(derivepoint(Z,hexstr))
+
+# ----- aliases -----
+
+import re
+def isvalidalias(alias):
+  if not re.match("^[A-Za-z][A-Za-z0-9_-]*",alias):
+    return false
+  return re.match("^[A-Za-z0-9_-]*[A-Za-z0-9]",alias):
+
+def normst(instring):
+   instring=instring.upper().replace("-","").replace("_","").replace("O","0").replace("I","1").replace("L","1")
+   instring=re.sub(r'([A-Z0-9])\1+', r'\1',instring)
+   return instring
+
+def aliastofilename(alias,versionin):
+   normalias="BCSIG_v"+str(versionin)+"_"+normst(alias)
+   return hash2idx(hash_160(normalias))
+
+# ----- print -----
+
+def printfile(filename):
+   ff=open(filename,'r')
+   for line in ff:
+      print line.rstrip()
+   ff.close();
+
+def printcert(cert,outtype):
+   try: 
+      alias=cert.signatures[0].value
+      versno=cert.signatures[0].algorithm.version
+   except:
+      alias=""
+   if alias is not "":
+      certfilename=aliastofilename(alias,versno)
+   else: 
+      certfilename="tmpfile" 
+   afile="/web/btcrypt.org/public_html/certs/"+certfilename+".acrt"
+   bfile="/web/btcrypt.org/public_html/certs/"+certfilename+".bcrt"
+   f=open(bfile,'wb')
+   f.write(cert.SerializeToString())
+   f.close()
+   asciicert=CertToAscii(cert)
+   f=open(afile,'wb')
+   f.write(asciicert)
+   f.close()
+   if outtype is "bincert":
+      filename=certfilename
+      # print "Content-type:application/octet-stream"
+      print "Content-Disposition: attachment; filename="+filename+".bcrt"
+      print "Content-type:text/plain\r\n\r"
+      out=cert.SerializeToString()
+      # print cert.SerializeToString()
+      import sys
+      sys.stdout.write(out.rstrip("\n"))
+   elif outtype is "asciicert":
+      print "Content-type:text/plain\r\n\r"
+      print asciicert.rstrip('\n')
+   else:
+      print "Content-type:text/html\r\n\r\n"
+      header="../public_html/header.php"
+      footer="../public_html/footer.php"
+      baseurl="http://btcrypt.org/"
+      printfile(header)
+      if alias is not "":
+         normalias="BCSIG_v"+str(versno)+"_"+normst(alias)
+         hashvalue=hash_160(normalias).encode('hex')
+         pubkeyval=hash2pubkey(hashvalue)
+         h160pubkey=hash_160(pubkeyval.decode('hex')).encode('hex')
+         print "normalized alias: "+normalias+"<p>"
+         print "hashed, normalize ALIAS: "+str(hashvalue)+"<p>"
+         print "hashed, normalize alias, multiplied with basepoint: "+str(pubkeyval)
+         print "Filename, which will be generated: "+str(h160pubkey)
+      print "<p>Binary Encoded saved as: <A HREF=\""+baseurl+"certs/"+certfilename+".bcrt\">/certs/"+certfilename+".bcrt</A></p>"
+      print "Simply check here: <A HREF=\""+baseurl+"cgi-bin/decode.cgi?fname="+certfilename+".bcrt\">/certs/"+certfilename+".bcrt</A></p>"
+      print "<PRE>"
+      print asciicert.rstrip('\n')
+      print "</PRE>"
+      print "<p>ASCII Encoded saved as: <A HREF=\""+baseurl+"certs/"+certfilename+".acrt\">/certs/"+certfilename+".acrt</A></p>"
+      print "Check in the certificate here:"
+      print "<p><form action=\""+baseurl+"cgi-bin/upload.cgi\" method=\"get\"><input type=\"hidden\" name=\"cert\" value=\""+certfilename+"\">"
+      print "<p><button type=\"submit\" class=\"btn\">Upload certificate</button></p></form>"
+      print "<H2>End of output</H2>"
+      printfile(footer)
+
+# ----- yaml -----
+      
+def getyaml(yamlin):
+    import yaml
+    yml=yaml.load(yamlin)
+    ver=yml['data']['version']
+    subject=yml['data']['subjectname']
+    contacts=[]
+    for i in yml['data']['contacts']:
+        contacts+=[[i['type'],i['value']]]
+    ty=yml['data']['paymentkeys'][0]['algorithm']['type']
+    pubkeys=[]
+    for i in yml['data']['paymentkeys']:
+        pubkeys+=i['value']
+    try:
+        alias=yml['signature']['value']
+    except:
+        alias=""
+    return ver,subject,contacts,ty,pubkeys,alias
+
+# ----- html -----
+
+def gethtml(inlist):
+   import cgi, cgitb 
+   import re
+   form = cgi.FieldStorage()
+   for i in inlist:
+      try: 
+         value=re.sub(r'[^a-zA-Z0-9_\n\r :\-@/,\.\"\{\}\[\]=]\+', '',form.getvalue(i))
+      except:
+         value=""
+      globals()[i] = value
+
+# ----- ascii -----
+
+def getascii(asc):
+   import re
+   stripped1=asc.split("\r\n\r\n")[1].split("=")[0][:-1]
+   stripped2=re.sub(r'\r\n','\n',stripped1)[:-1]
+   bina=binascii.a2b_base64(stripped2)
+   return bina
+
+# def gethtml(inlist):
+#    import cgi, cgitb 
+#    import re
+#    form = cgi.FieldStorage()
+#    for i in inlist:
+#       value=re.sub(r'\W+', '',form.getvalue(i))
+#       globals()[i] = value
+
